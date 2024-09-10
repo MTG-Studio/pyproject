abb3eaf93ca8c405d6044f6577a91057ca976ac7
Talgat <mtg2006@mail.ru>
2024-09-10 19:02:11 +0500

    Создание тестов для кода проекта

--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,3 @@
-import os
-
-
 def mask_account_card(account_card: str) -> str:
     """Маскирует и отображает номер карты в формате 'Visa Platinum 7000 79** **** 6361'
     или номер счета в формате 'Счет **4305'"""
@@ -31,3 +28,17 @@ def get_date(date: str) -> str:
     """принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407"
     и возвращает строку с датой в формате "ДД.ММ.ГГГГ"("11.03.2024")"""
     return f"{date[8:10]}.{date[5:7]}.{date[:4]}"
+
+
+# Проверка работы функции mask_account_card:
+print(mask_account_card("Maestro 1596837868705199"))
+print(mask_account_card("Счет 64686473678894779589"))
+print(mask_account_card("MasterCard 7158300734726758"))
+print(mask_account_card("Счет 35383033474447895560"))
+print(mask_account_card("Visa Classic 6831982476737658"))
+print(mask_account_card("Visa Platinum 8990922113665229"))
+print(mask_account_card("Visa Gold 5999414228426353"))
+print(mask_account_card("Счет 73654108430135874305"))
+
+# Проверка работы функции get_date:
+print(get_date("2024-03-11T02:26:18.671407"))
